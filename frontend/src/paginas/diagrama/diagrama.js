09f51dc1b582c155f5eac886bf91c678dee37843
l-luz <lyraluz@yahoo.com.br>
2023-10-30 00:51:50 -0300

    excalidraw personalizado
    view mode
    controle da api

--- frontend/src/paginas/diagrama/diagrama.js
+++ frontend/src/paginas/diagrama/diagrama.js
@@ -1,43 +1,51 @@
 import { Component } from "react";
-import { Excalidraw, MainMenu, defaultLang } from "@excalidraw/excalidraw";
+import { Excalidraw, MainMenu, defaultLang, WelcomeScreen, Footer } from "@excalidraw/excalidraw";
 
 defaultLang.code = "pt-PT";
-window.EXCALIDRAW_ASSET_PATH = "/";
 
 class DiagramaManager extends Component {
     constructor(props) {
         super(props);
         this.state = {
             etapaId: 'node0',
+            viewMode: false
         };
     }
 
-    handleOnChange = (excalidrawElements, appState, files) => {
-        // const { cy, etapas, etapaAtual, excalidrawApi, setAtualElements } = this.props;
-        // const {etapaId} = this.state;
-        // console.log("aqui")
-        // const atual = cy.nodes(".atual")[0];
-
-        // console.log("eid " + etapaId + " x  " + "atual " + etapaAtual );
-        // console.log("onchange")
-        // console.log(atual.data('elements'))
-        // console.log(excalidrawElements);
-        // console.log(appState);
-        // console.log(files);
-    }
-
+    
     render() {
-        const {setExcalidrawApi} = this.props;
+        const {setExcalidrawApi, toggleViewMode, viewMode} = this.props;
         return (
-            <Excalidraw onChange={this.handleOnChange} ref={(api) => {setExcalidrawApi(api)}  } >
-                <MainMenu>
+            <Excalidraw onChange={this.handleOnChange} viewModeEnabled={viewMode} ref={(api) => {setExcalidrawApi(api)}} 
+            
+            renderTopRightUI={() => {
+                return (
+                  <button
+                    style={{
+                      background: "#70b1ec",
+                      border: "none",
+                      color: "#fff",
+                      width: "max-content",
+                      fontWeight: "bold",
+                    }}
+                    onClick={() => {toggleViewMode() }}
+                  >
+                    Apresentação
+                  </button>
+                );
+              }}
+            >
+      
+                {/* <MainMenu>
                     <MainMenu.Item onSelect={() => window.alert("Item1")}>
                         Item1
                     </MainMenu.Item>
                     <MainMenu.Item onSelect={() => window.alert("Item2")}>
                         Item 2
                     </MainMenu.Item>
-                </MainMenu>
+                </MainMenu> */}
+                <Footer>
+        </Footer>
             </Excalidraw>
         );
     };
