f237b33761ae21e3ac9160aa6bf70c5751fa7deb
l-luz <lyraluz@yahoo.com.br>
2023-12-08 20:59:52 -0300

    fix - encriptar senhas no admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -3,6 +3,12 @@ from .models import *
 
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'username')
+    def save_model(self, request, obj, form, change):
+        if 'password' in form.cleaned_data and form.cleaned_data['password']:
+            obj.set_password(form.cleaned_data['password'])
+
+        super().save_model(request, obj, form, change)
+
 
 class TurmaAdmin(admin.ModelAdmin):
     list_display = ('responsavel', 'periodo', 'disciplina')
@@ -15,8 +21,6 @@ class GerenciaAdmin(admin.ModelAdmin):
 
 class DisciplinaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'nome')
-
-# Register your models here.
 
 admin.site.register(User, UsuarioAdmin)
 admin.site.register(Turma, TurmaAdmin)
