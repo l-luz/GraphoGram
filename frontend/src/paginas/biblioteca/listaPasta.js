5c41419b611d5c9b7ca2d152c6075605798466b2
l-luz <lyraluz@yahoo.com.br>
2023-12-10 00:38:01 -0300

    voltando para componente
    algumas coisas quebraram

--- frontend/src/paginas/biblioteca/listaPasta.js
+++ frontend/src/paginas/biblioteca/listaPasta.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import axios from "axios";
 import { FiMoreVertical, FiTrash2 } from "react-icons/fi";
 import { TbPlus } from "react-icons/tb";
@@ -67,10 +67,7 @@ function ItemDiagrama({ item, refresh }) {
         refresh();
     };
 
-    const options = [
-        'Apagar',
-        'Descrição',
-    ];
+
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -90,17 +87,14 @@ function ItemDiagrama({ item, refresh }) {
                 padding: '0 20px',
                 boxSizing: 'border-box',
                 width: '192px',
-                height: '48px',
+                height: '50px',
             }}>
 
             <Grid container alignItems="center" textalign='center' justify="space-between">
                 <Grid item xs={10}>
-                    <br></br>
-                    <Tooltip tittle={item.descricao}>
-                        <Link href={`diagrama/${item.id}`} color="inherit" underline="none">
+                        <Button href={`diagrama/${item.id}`} color="inherit" underline="none">
                             {item.titulo}
-                        </Link>
-                    </Tooltip>
+                        </Button>
 
                 </Grid>
                 <Grid item xs={2}>
@@ -131,13 +125,18 @@ function ItemDiagrama({ item, refresh }) {
                     width: '20ch',
                 }}
             >
-                {options.map((option) => (
+                <Tooltip  title={item.descricao} placement="top">
                     <MenuItem
-                        key={option}
-                        onClick={option === 'Apagar' ? handleDelete : handleClose}>
-                        {option}
+                        key="descricao"
+                        onClick={handleClose}>
+                            Descricao
                     </MenuItem>
-                ))}
+                    </Tooltip>
+                    <MenuItem
+                        key="apagar"
+                        onClick={handleDelete}>
+                        Apagar
+                    </MenuItem>
             </Menu>
             {deleteDial ?
                 (
@@ -149,201 +148,79 @@ function ItemDiagrama({ item, refresh }) {
 }
 
 
-// class Biblioteca extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             modal: false,
-//             pastas: [],
-//             diagramas: [],
-//             id: "",
-//             activeItem: {
-//                 nome: ""
-//             },
-//         };
-//     }
+class Biblioteca extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false,
+            pastas: [],
+            diagramas: [],
+            id: "",
+            activeItem: {
+                nome: ""
+            },
+        };
+    }
 
-//     refreshList = () => {
-//         axios
-//             .get(`/api/pastas/${this.state.id}`)
-//             .then((res) => {
-//                 this.setState({
-//                     pastas: res.data.pastas,
-//                     diagramas: res.data.diagramas
-//                 });
-//             })
-//             .catch((err) => console.log(err));
-//     };
-
-//     componentDidMount() {
-//         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
-//         const path = window.location.href.split('/');
-//         let pageId = Number(path[path.length - 1]);
-//         if (isNaN(pageId)) {
-//             pageId = '';
-//         }
-
-//         this.setState({ id: pageId }, () => {
-//             this.refreshList();
-//         });
-
-//     }
-
-//     toggle = () => {
-//         this.setState({ modal: !this.state.modal });
-//     };
-
-//     handleSubmit = (item) => {
-//         this.toggle();
-//         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
-//         item["descendente"] = this.state.id;
-
-//         if (item.id) {
-//             axios
-//                 .put(`/api/pastas/${item.id}/`, item)
-//                 .then((res) => this.refreshList());
-//             return;
-//         }
-//         axios
-//             .post("/api/pastas/", item)
-//             .then((res) => this.refreshList());
-//         console.log("adicionando pasta");
-
-//     };
-
-//     criarItem = () => {
-//         const item = {
-//             nome: ""
-//         };
-
-//         this.setState({ activeItem: item, modal: !this.state.modal });
-//     };
-
-//     render() {
-//         const { pastas, diagramas } = this.state;
-//         return (
-//             <>
-//                 <Grid container alignItems="center">
-//                     <Grid item xs={6}>
-//                         <h1 style={{ marginlE: '20' }}>Biblioteca</h1>
-//                     </Grid>
-//                     {/* <Grid item xs={2}>
-//                         <Button variant="outlined" onClick={this.criarItem}>
-//                             <TbPlus size={20} /> Nova Pasta
-//                         </Button>
-//                     </Grid> */}
-//                 </Grid>
-//                 {pastas ? (
-//                     <div className="pastas" >
-//                         <h2 style={{ marginlE: '20' }}>Pastas</h2>
-//                         <Stack
-//                             className="folder-list"
-//                             marginTop={5}
-//                             marginBottom={8}
-//                             marginLeft={5}
-//                             spacing={{ xs: 1, sm: 5 }}
-//                             direction="row"
-//                             useFlexGap
-//                             flexWrap="wrap"
-//                         >
-//                             {pastas ? pastas.map((folder) => (
-//                                 <ItemPasta key={folder.id} item={folder} refresh={this.refreshList} />
-//                             )) : null}
-//                         </Stack>
-//                     </div>
-//                 ) : null}
-//                 <hr />
-//                 {diagramas ? (
-//                     <div className="diagramas">
-//                         <h2 style={{ marginlE: '20' }}>Diagramas</h2>
-//                         <Stack
-//                             className="diagram-list"
-//                             marginTop={5}
-//                             marginLeft={5}
-//                             spacing={{ xs: 1, sm: 5 }}
-//                             direction="row"
-//                             useFlexGap
-//                             flexWrap="wrap"
-//                         >
-//                             {diagramas ? diagramas.map((diagram) => (
-//                                 <ItemDiagrama key={diagram.id} item={diagram} refresh={this.refreshList} />
-//                             )) : null}
-//                         </Stack>
-//                     </div>
-//                 ) : null}
-//                 {this.state.modal ? (
-//                     <AddPasta
-//                         activeItem={this.state.activeItem}
-//                         toggle={this.toggle}
-//                         onSave={this.handleSubmit}
-//                     />
-//                 ) : null}
-//             </>
-//         );
-//     }
-// };
-
-
-const Biblioteca = (props) => {
-    const [modal, setModal] = useState(false);
-    const [pastas, setPastas] = useState([]);
-    const [diagramas, setDiagramas] = useState([]);
-    const [id, setId] = useState('');
-    const [activeItem, setActiveItem] = useState({nome: ''});
-
-    const refreshList = () => {
-        if (id) {
-            axios
-            .get(`/api/pastas/${id}`)
+    refreshList = () => {
+        axios
+            .get(`/api/pastas/${this.state.id}`)
             .then((res) => {
-                setPastas(res.data.pastas);
-                setDiagramas(res.data.diagramas);
+                this.setState({
+                    pastas: res.data.pastas,
+                    diagramas: res.data.diagramas
+                });
+                console.table(res.data.pastas)
             })
             .catch((err) => console.log(err));
-
-        }
     };
 
-    useEffect (() => {
+    componentDidMount() {
         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
         const path = window.location.href.split('/');
         let pageId = Number(path[path.length - 1]);
         if (isNaN(pageId)) {
             pageId = '';
         }
-        setId(pageId);
-        refreshList();
-    }, []);
 
-    const toggle = () => {
-        setModal(!modal);
+        this.setState({ id: pageId }, () => {
+            this.refreshList();
+        });
+
+    }
+
+    toggle = () => {
+        this.setState({ modal: !this.state.modal });
     };
 
-    const handleSubmit = (item) => {
-        toggle();
+    handleSubmit = (item) => {
+        this.toggle();
         axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem("access_token")}`;
-        item["descendente"] = id;
+        item["descendente"] = this.state.id;
 
         if (item.id) {
             axios
                 .put(`/api/pastas/${item.id}/`, item)
-                .then((res) => refreshList());
+                .then((res) => this.refreshList());
             return;
         }
         axios
             .post("/api/pastas/", item)
-            .then((res) => refreshList());
+            .then((res) => this.refreshList());
         console.log("adicionando pasta");
+
     };
 
-    const criarItem = () => {
+    criarItem = () => {
         const item = {
             nome: ""
         };
-        setActiveItem(item);
-        setModal(!modal);
+
+        this.setState({ activeItem: item, modal: !this.state.modal });
     };
 
+    render() {
+        const { pastas, diagramas } = this.state;
         return (
             <>
                 <Grid container alignItems="center">
@@ -351,12 +228,12 @@ const Biblioteca = (props) => {
                         <h1 style={{ marginlE: '20' }}>Biblioteca</h1>
                     </Grid>
                     {/* <Grid item xs={2}>
-                        <Button variant="outlined" onClick={criarItem}>
+                        <Button variant="outlined" onClick={this.criarItem}>
                             <TbPlus size={20} /> Nova Pasta
                         </Button>
                     </Grid> */}
                 </Grid>
-                {pastas ? (
+                {pastas[0] ? (
                     <div className="pastas" >
                         <h2 style={{ marginlE: '20' }}>Pastas</h2>
                         <Stack
@@ -376,7 +253,7 @@ const Biblioteca = (props) => {
                     </div>
                 ) : null}
                 <hr />
-                {diagramas ? (
+                {diagramas[0] ? (
                     <div className="diagramas">
                         <h2 style={{ marginlE: '20' }}>Diagramas</h2>
                         <Stack
@@ -394,16 +271,18 @@ const Biblioteca = (props) => {
                         </Stack>
                     </div>
                 ) : null}
-                {modal ? (
+                {this.state.modal ? (
                     <AddPasta
-                        activeItem={activeItem}
-                        toggle={toggle}
-                        onSave={handleSubmit}
+                        activeItem={this.state.activeItem}
+                        toggle={this.toggle}
+                        onSave={this.handleSubmit}
                     />
                 ) : null}
             </>
         );
+    }
 };
+
 
 
 export default Biblioteca;
